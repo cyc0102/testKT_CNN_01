cc2406c3c24c7f68f0c1d29f02cbd99e1328bee5
bryan <cyc0102@gmail.com>
2018-07-04 10:30:43 +0800

    skipe padding=same

--- keras_mnist_cnn.py
+++ keras_mnist_cnn.py
@@ -16,13 +16,13 @@ from keras.layers import Dense,Dropout,Flatten,Conv2D,MaxPooling2D
 model = Sequential()
 model.add(Conv2D(filters=16,
                  kernel_size=(5,5),
-                 padding='same',
+#                 padding='same',
                  input_shape=(28,28,1), 
                  activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))    
 model.add(Conv2D(filters=36,
                  kernel_size=(5,5),
-                 padding='same',
+#                 padding='same',
                  activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2))) 
 model.add(Dropout(0.25))
